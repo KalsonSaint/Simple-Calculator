afcf0a7bf7e7fcf4f0fb8f34f6a659589ab2447a
KalsonSaint <saintkalson@gmail.com>
2019-10-05 10:07:26 +0100

    Petit Update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,39 @@
 const getHistory = () => {
     return document.getElementById("history-value").innerText;
 }
-// const printHistory = num => {
-//     document.getElementById("history-value").innerText = num;
-// }
-
-alert(getHistory());
+const printHistory = num => {
+    document.getElementById("history-value").textContent = num;
+}
+const getOutput = () => {
+    return document.querySelector('.#output-value').innerText;
+}
+const printOutput = num => {
+    if(num == ""){
+        document.querySelector('#output-value').innerText = num;
+    } else{
+        document.querySelector('#output-value').innerText = getFormattedNumber(num);
+    }
+}
+const getFormattedNumber = num => {
+    let n = Number(num).toLocaleString("en");
+    return n;
+}
+const reverseNumberFormat = num => {
+    return Number(num.replace(/,/g,''));
+}
+let operator = document.querySelectorAll('.operator');
+operator.forEach(e => {
+    e.addEventListener('click', function() {
+        
+    })
+});
+let number = document.querySelectorAll('.number');
+number.forEach(e => {
+    e.addEventListener('click', function() {
+        let output = reverseNumberFormat(getOutput());
+        if(!(output=NaN)){
+            output=output+this.id;
+            printOutput(output);
+        }
+    })
+});
